49e8e2c96bd4ac10ab9fa1953a555396b3716195
Moh. Rofiqi <88334099+ficilabs@users.noreply.github.com>
2025-09-14 06:41:52 +0000

    add use head

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,6 +2,15 @@
 import MyPage from '~/components/MyPage.vue'
 import MyHero from '~/components/MyHero.vue'
 
+useSeoMeta({
+  title: 'My Portfolio',
+  ogTitle: 'My Portfolio',
+  description: 'This is my amazing site, let me tell you all about it.',
+  ogDescription: 'This is my amazing site, let me tell you all about it.',
+  ogImage: 'maskable-icon-512x512.png',
+  twitterCard: 'summary_large_image',
+})
+
 const pageData = {
   title: "Hi! <br> I'm Rofiqi",
   content: {
@@ -10,8 +19,8 @@ const pageData = {
       title: "Frontend Developer",
       description: "I’m passionate about building <strong>smart</strong> and <strong>impactful systems</strong> powered by <strong>web development</strong>, <strong>artificial intelligence</strong> and <strong>IoT</strong>.",
       buttons: [
-        { id: 1, text: "Discover my projects", isLink: true, link: "/about" },
-        { id: 2, text: "Who am i?", isLink: true, link: "/projects" },
+        { id: 1, text: "Discover my projects", isLink: true, link: "/projects" },
+        { id: 2, text: "Who am i?", isLink: true, link: "/about" },
       ],
       images: ["/images/profile.png"],
     }
